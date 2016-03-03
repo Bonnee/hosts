743115126895f0aca442820d906c0d0fde976fa6
SuperBonny <bonora.matteo@gmail.com>
2016-03-03 20:41:02 +0100

    Total rewrite

--- Hosts/Program.cs
+++ Hosts/Program.cs
@@ -10,35 +10,21 @@ namespace Hosts
 	{
 		public static void Main (string[] args)
 		{
-			List<string> hosts = new List<string> ();
+
 			string[] files = new string[] {
 				"http://adaway.org/hosts.txt",
 				"http://winhelp2002.mvps.org/hosts.txt",
-				"http://hosts-file.net/ad_servers.txt"
+				"http://hosts-file.net/ad_servers.txt",
+				"http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
 			};
-			WebClient cl = new WebClient ();
 
-			for (int i = 0; i < files.Length; i++) {
-				foreach (string host in cl.DownloadString (files [i]).Split('\n')) {
-					if (isRelevant (host)) {
-						hosts.Add (Clean (host).Split (',') [1]);
-						Console.WriteLine (Clean (host).Split (',') [1]);
-					}
-				}
-			}
-		}
+			hosts h = new hosts ("0.0.0.0", files);
 
-		static bool isRelevant (string line)
-		{
-			line = Clean (line);
-			if (line.StartsWith ("#") || line.Length == 0 || line.StartsWith ("\r"))
-				return false;
-			return true;
-		}
-
-		static string Clean (string s)
-		{
-			return s.Replace (" ", ",").Replace ("\t", ",");
+			Console.WriteLine ("Downloading from sources...");
+			h.GetHosts ();
+			Console.WriteLine ("Merging and saving...");
+			h.Merge ("/etc/hosts");
+			Console.WriteLine ("Done.");
 		}
 	}
 }
