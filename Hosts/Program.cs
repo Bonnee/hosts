ad2402f86a94cee312d0f4a45e1d097b4afb45c9
SuperBonny <bonora.matteo@gmail.com>
2016-03-04 15:32:42 +0100

    Add hosts sorting

--- Hosts/Program.cs
+++ Hosts/Program.cs
@@ -12,8 +12,8 @@ namespace Hosts
 				"http://winhelp2002.mvps.org/hosts.txt",
 				"http://hosts-file.net/ad_servers.txt",
 				"http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
-                "http://someonewhocares.org/hosts/hosts",
-                "http://hosts-file.net/download/hosts.txt"
+				"http://someonewhocares.org/hosts/hosts",
+				"http://hosts-file.net/download/hosts.txt"
 			};
 
 			hosts h = new hosts ("0.0.0.0", files);
@@ -21,8 +21,8 @@ namespace Hosts
 			Console.WriteLine ("Downloading from sources...");
 			h.GetHosts ();
 			Console.WriteLine ("Merging and saving...");
-			if(h.Merge ("E:/hosts"))
-                Console.WriteLine ("Done.");
+			if (h.Merge ("/etc/hosts"))
+				Console.WriteLine ("Done.");
 		}
 	}
 }
--- Hosts/hosts.cs
+++ Hosts/hosts.cs
@@ -2,100 +2,93 @@
 using System.Collections.Generic;
 using System.Net;
 using System.IO;
+using System.Linq;
 
 namespace Hosts
 {
-    public class hosts
-    {
-        List<string[]> entries;
+	public class hosts
+	{
+		List<string[]> entries;
 
-        public List<string[]> Hosts { get { return entries; } }
+		public List<string[]> Hosts { get { return entries; } }
 
-        string addr;
+		string addr;
 
-        public string Address
-        {
-            get { return addr; }
-            set { addr = value; }
-        }
+		public string Address {
+			get { return addr; }
+			set { addr = value; }
+		}
 
-        public string Header { get; set; }
+		public string Header { get; set; }
 
-        public List<string> Sources { get; set; }
+		public List<string> Sources { get; set; }
 
-        public hosts(string address, string[] sources)
-        {
-            entries = new List<string[]>();
-            Address = address;
-            Sources = new List<string>(sources);
-        }
+		public hosts (string address, string[] sources)
+		{
+			entries = new List<string[]> ();
+			Address = address;
+			Sources = new List<string> (sources);
+		}
 
-        public void GetHosts()
-        {
-            WebClient cl = new WebClient();
-            cl.Proxy.Credentials = CredentialCache.DefaultCredentials;
-            for (int i = 0; i < Sources.Count; i++)
-            {
-                Console.WriteLine(Sources[i]);
-                try
-                {
-                    string[] host = cl.DownloadString(Sources[i]).Split('\n');
-                    for (int k = 0; k < host.Length; k++)
-                    {
-                        if (isRelevant(host[k]))
-                            entries.Add(new string[] { addr, Clean(host[k]) });
-                    }
-                }
-                catch (WebException e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
-        }
+		public void GetHosts ()
+		{
+			WebClient cl = new WebClient ();
+			cl.Proxy.Credentials = CredentialCache.DefaultCredentials;
+			for (int i = 0; i < Sources.Count; i++) {
+				Console.WriteLine (Sources [i]);
+				try {
+					string[] host = cl.DownloadString (Sources [i]).Split ('\n');
+					for (int k = 0; k < host.Length; k++) {
+						if (isRelevant (host [k]))
+							entries.Add (new string[] { addr, Clean (host [k]) });
+					}
+				} catch (WebException e) {
+					Console.WriteLine (e.Message);
+				}
+			}
 
-        bool isRelevant(string line)
-        {
-            line = line.Replace(" ", ",");
-            if (line.StartsWith("#") || line.Length == 0 || line.StartsWith("\r") || line.Contains("localhost") || line.Contains("::1"))
-                return false;
-            return true;
-        }
+			entries = entries.OrderBy (o => o [1]).ToList ();
+		}
 
-        string Clean(string line)
-        {
-            return line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)[1];
-        }
+		bool isRelevant (string line)
+		{
+			line = line.Replace (" ", ",");
+			if (line.StartsWith ("#") || line.Length == 0 || line.StartsWith ("\r") || line.Contains ("localhost") || line.Contains ("::1"))
+				return false;
+			return true;
+		}
 
-        string CreateHeader()
-        {
-            string header = "# Host file courtesy of SuperBonny.\n# This list was updated on " + DateTime.Now.ToShortDateString() + " at " + DateTime.Now.ToShortTimeString() + "\n# There are " + entries.Count + " hosts present.\n# These are the sources:\n#\n# ";
-            foreach (string s in Sources)
-            {
-                header += s + "\n# ";
-            }
-            header += "\n127.0.0.1 localhost\n::1 localhost\n\n# [START OF GENERATED ENTRIES]\n#\n";
-            return header;
-        }
+		string Clean (string line)
+		{
+			return line.Split (new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries) [1];
+		}
 
-        public bool Merge(string filename)
-        {
-            try
-            {
-                if (File.Exists(filename))
-                    File.Delete(filename);
+		string CreateHeader ()
+		{
+			string header = "# Host file courtesy of SuperBonny.\n# This list was updated on " + DateTime.Now.ToShortDateString () + " at " + DateTime.Now.ToShortTimeString () + "\n# There are " + entries.Count + " hosts present.\n# These are the sources:\n#\n# ";
+			foreach (string s in Sources) {
+				header += s + "\n# ";
+			}
+			header += "\n127.0.0.1 localhost\n::1 localhost\n\n# [START OF GENERATED ENTRIES]\n#\n";
+			return header;
+		}
 
-                StreamWriter sw = new StreamWriter(filename);
-                sw.Write(CreateHeader());
-                foreach (string[] host in entries)
-                    sw.Write(host[0] + "\t" + host[1] + "\n");
-                sw.Close();
-                return true;
-            }
-            catch (UnauthorizedAccessException e)
-            {
-                Console.WriteLine("You have no permission to write in " + filename + ", moron.");
-                return false;
-            }
-        }
-    }
+		public bool Merge (string filename)
+		{
+			try {
+				if (File.Exists (filename))
+					File.Delete (filename);
+
+				StreamWriter sw = new StreamWriter (filename);
+				sw.Write (CreateHeader ());
+				foreach (string[] host in entries)
+					sw.Write (host [0] + "\t" + host [1] + "\n");
+				sw.Close ();
+				return true;
+			} catch (UnauthorizedAccessException e) {
+				Console.WriteLine ("You have no permission to write in " + filename + ", moron.");
+				return false;
+			}
+		}
+	}
 }
