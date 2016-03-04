5c1f6f3577812faf8f5471cc756b58e8b36f5bb7
SuperBonny <bonora.matteo@gmail.com>
2016-03-04 13:08:04 +0100

    Add source

--- Hosts/Program.cs
+++ Hosts/Program.cs
@@ -12,7 +12,8 @@ namespace Hosts
 				"http://winhelp2002.mvps.org/hosts.txt",
 				"http://hosts-file.net/ad_servers.txt",
 				"http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
-                "http://someonewhocares.org/hosts/hosts"
+                "http://someonewhocares.org/hosts/hosts",
+                "http://hosts-file.net/download/hosts.txt"
 			};
 
 			hosts h = new hosts ("0.0.0.0", files);
