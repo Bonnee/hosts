9bd371a9b2cb1e3de6e6fc3814f388deceed1c12
SuperBonny <bonora.matteo@gmail.com>
2016-03-04 12:40:41 +0100

    Fix junk code

--- Hosts/Program.cs
+++ Hosts/Program.cs
@@ -12,8 +12,7 @@ namespace Hosts
 				"http://winhelp2002.mvps.org/hosts.txt",
 				"http://hosts-file.net/ad_servers.txt",
 				"http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
-                "http://someonewhocares.org/hosts/hosts",
-                "http://sitoacaso"
+                "http://someonewhocares.org/hosts/hosts"
 			};
 
 			hosts h = new hosts ("0.0.0.0", files);
