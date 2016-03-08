592b71b8074d5c31a7d1a4078605154970c75712
SuperBonny <bonora.matteo@gmail.com>
2016-03-08 11:16:27 +0100

    Revert "Initial (NOT WORKING) multi-threaded code"
    This reverts commit 4737ba053bbb609d12a53513ca7b57c366ca5469.

--- Hosts/hosts.cs
+++ Hosts/hosts.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Net;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hosts
 {
 	public class HostsManager
 	{
-        static readonly object locker = new object();
 		List<string[]> entries;
 
 		public List<string[]> Hosts { get { return entries; } }
@@ -43,6 +41,7 @@ namespace Hosts
 			WebClient cl = new WebClient ();
 			cl.Proxy.Credentials = CredentialCache.DefaultCredentials;
 
+			string[] entry;
 			int index;
 
 			foreach (string source in Sources) {
@@ -54,8 +53,16 @@ namespace Hosts
 					Console.Write ("Adding...");
 					index = Console.CursorLeft;
 
-					for (int i = 0; i < host.Length - 1; i++) {
-                        new Task<bool>(() => CheckAndAdd(host[i])).Start();
+					for (int i = 0; i < host.Length; i++) {
+						
+						if (isRelevant (host [i])) {
+							entry = new string[] { addr, Clean (host [i]) };
+							if (!Exists (entries, entry)) {
+								entries.Add (entry);
+								Console.CursorLeft = index;
+								Console.Write (i + 1 + "/" + host.Length);
+							}
+						}
 					}
 					Console.Write ("...Done.\n");
 
@@ -67,24 +74,6 @@ namespace Hosts
 
 			entries = entries.OrderBy (o => o [1]).ToList ();
 		}
-
-        bool CheckAndAdd(string host)
-        {
-            if (isRelevant(host))
-            {
-                string[] entry = new string[] { addr, Clean(host) };
-                lock (locker)
-                {
-                    if (!Exists(entries, entry))
-                    {
-                        entries.Add(entry);
-                        return true;
-                    }
-                }
-                return false;
-            }
-            return false;
-        }
 
 		bool Exists (List<string[]> list, string[] arr)
 		{
