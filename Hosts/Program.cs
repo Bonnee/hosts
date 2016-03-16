2b167eb123548154ae65c11fbdab90bfe1eac5b0
SuperBonny <bonora.matteo@gmail.com>
2016-03-16 18:21:38 +0100

    Core rework

--- Hosts/Program.cs
+++ Hosts/Program.cs
@@ -3,60 +3,54 @@ using System.IO;
 
 namespace Hosts
 {
-    class MainClass
-    {
-        static string dest;
-        static HostsManager h;
+	class MainClass
+	{
+		static string dest;
+		static HostsManager h;
 
-        public static void Main(string[] args)
-        {
-            h = new HostsManager();
-            Load(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/hosts/settings");
+		public static void Main (string[] args)
+		{
+			h = new HostsManager ();
+			Load (Environment.GetFolderPath (Environment.SpecialFolder.ApplicationData) + @"/hosts/settings");
 
-            Console.WriteLine("Downloading from sources...");
-            if (h.GetHosts())
-            {
-                Console.WriteLine("Merging and saving...");
-                if (h.Write(dest))
-                    Console.WriteLine("Done.");
-            }
-        }
+			Console.WriteLine ("Downloading from sources...");
+			if (h.GetHosts ()) {
+				if (h.Write (dest))
+					Console.WriteLine ("Done.");
+			}
+		}
 
-        static void Load(string path)
-        {
-            path = Path.GetFullPath(path);
-            if (File.Exists(path))
-            {
-                Console.WriteLine("Loading config from: " + path);
-                string[] settings = File.ReadAllText(path).Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+		static void Load (string path)
+		{
+			path = Path.GetFullPath (path);
+			if (File.Exists (path)) {
+				Console.WriteLine ("Loading config from: " + path);
+				string[] settings = File.ReadAllText (path).Split (new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-                foreach (string setting in settings)
-                {
-                    string[] s = setting.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+				foreach (string setting in settings) {
+					string[] s = setting.Split (new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    if (s[0] == "source")
-                        h.Sources.Add(s[1]);
-                    else if (s[0] == "redirect")
-                        h.Address = s[1];
-                    else if (s[0] == "destination")
-                        dest = s[1];
-                }
-            }
-            else
-            {
-                Console.WriteLine("Loading default configuration in " + path);
-                LoadDefaults(path);
-            }
-        }
+					if (s [0] == "source")
+						h.Sources.Add (s [1]);
+					else if (s [0] == "redirect")
+						h.Address = s [1];
+					else if (s [0] == "destination")
+						dest = s [1];
+				}
+			} else {
+				Console.WriteLine ("Loading default configuration in " + path);
+				LoadDefaults (path);
+			}
+		}
 
-        static void LoadDefaults(string path)
-        {
-            string file = "destination /etc/hosts\nredirect 0.0.0.0\n# Pro tip: order the sources by file size from the biggest to the smallest.\nsource http://hosts-file.net/ad_servers.txt\nsource http://winhelp2002.mvps.org/hosts.txt\nsource http://someonewhocares.org/hosts/hosts\nsource http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext\nsource http://adaway.org/hosts.txt";
-            if (!Directory.Exists(Directory.GetParent(path).ToString()))
-                Directory.CreateDirectory(Directory.GetParent(path).ToString());
-            File.WriteAllText(path, file);
+		static void LoadDefaults (string path)
+		{
+			string file = "destination /etc/hosts\nredirect 0.0.0.0\n# Pro tip: order the sources by file size from the biggest to the smallest.\nsource http://hosts-file.net/ad_servers.txt\nsource http://winhelp2002.mvps.org/hosts.txt\nsource http://someonewhocares.org/hosts/hosts\nsource http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext\nsource http://adaway.org/hosts.txt";
+			if (!Directory.Exists (Directory.GetParent (path).ToString ()))
+				Directory.CreateDirectory (Directory.GetParent (path).ToString ());
+			File.WriteAllText (path, file);
 
-            Load(path);
-        }
-    }
+			Load (path);
+		}
+	}
 }
--- Hosts/hosts.cs
+++ Hosts/hosts.cs
@@ -3,175 +3,196 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Hosts
 {
-    public class HostsManager
-    {
-        List<string[]> entries;
-        public List<string[]> Hosts { get { return entries; } }
+	public class HostsManager
+	{
+		List<string[]> entries;
 
-        string addr;
-        public string Address
-        {
-            get { return addr; }
-            set { addr = value; }
-        }
+		public List<string[]> Hosts { get { return entries; } }
 
-        public string Header { get; set; }
+		string addr;
 
-        public List<string> Sources { get; set; }
+		public string Address {
+			get { return addr; }
+			set { addr = value; }
+		}
 
-        public HostsManager()
-        {
-            entries = new List<string[]>();
-            Sources = new List<string>();
-        }
+		public string Header { get; set; }
 
-        public HostsManager(string address, string[] sources)
-        {
-            entries = new List<string[]>();
-            Address = address;
-            Sources = new List<string>(sources);
-        }
+		public List<string> Sources { get; set; }
 
-        /// <summary>
-        /// Fetches the hosts files from all the sources and adds them to "Hosts" list
-        /// </summary>
-        /// <returns>If the operation succedded.</returns>
-        public bool GetHosts()
-        {
-            WebClient cl = new WebClient();
-            cl.Proxy.Credentials = CredentialCache.DefaultCredentials;
+		public HostsManager ()
+		{
+			entries = new List<string[]> ();
+			Sources = new List<string> ();
+		}
 
-            string[] entry;
-            int index;
-            int errors = 0;
+		public HostsManager (string address, string[] sources)
+		{
+			entries = new List<string[]> ();
+			Address = address;
+			Sources = new List<string> (sources);
+		}
 
-            foreach (string source in Sources)
-            {
-                Console.Write(Sources.IndexOf(source) + 1 + "/" + Sources.Count + " " + source + "...");
-                try
-                {
-                    string[] host = cl.DownloadString(source).Split('\n');
-                    Console.Write("Adding...");
-                    index = Console.CursorLeft;
-                    int add = 0;
+		/// <summary>
+		/// Fetches the hosts files from all the sources and adds them to "Hosts" list
+		/// </summary>
+		/// <returns>If the operation succedded.</returns>
+		public bool GetHosts ()
+		{
+			List<List<string[]>> partial = new List<List<string[]>> ();
 
-                    for (int i = 0; i < host.Length; i++)
-                    {
-                        if (isRelevant(host[i]))
-                        {
-                            entry = new string[] { addr, Clean(host[i]) };
-                            if (!Exists(entries, entry))
-                            {
-                                entries.Add(entry);
-                                Console.CursorLeft = index;
-                                add++;
-                                Console.Write(add + "/" + host.Length);
-                            }
-                        }
-                    }
-                    Console.Write("...Done.\n");
-                    add = 0;
+			int index;
+			int errors = 0;
 
-                }
-                catch (WebException e)
-                {
-                    errors++;
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine(e.Message);
-                    Console.ResetColor();
-                }
-            }
-            if (errors >= Sources.Count)
-                return false;
-            entries = entries.OrderBy(o => o[1]).ToList();
-            return true;
-        }
+			//List<Task> worker = new List<Task> ();
 
-        /// <summary>
-        /// Check if the given host already exists in the given list
-        /// </summary>
-        /// <param name="list">The list to check</param>
-        /// <param name="arr">The element to verify</param>
-        /// <returns></returns>
-        bool Exists(List<string[]> list, string[] arr)
-        {
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (list[i][1] == arr[1])
-                    return true;
-            }
-            return false;
-        }
+			foreach (string source in Sources) {
+				//worker.Add (new Task (() => {
 
-        /// <summary>
-        /// Checks wether the string given is a valid host entry
-        /// </summary>
-        /// <param name="line">The line to check</param>
-        /// <returns>If the line is valid</returns>
-        bool isRelevant(string line)
-        {
-            line = line.Replace(" ", ",");
-            if (line.StartsWith("#") || line.Length == 0 || line.StartsWith("\r") || line.Contains("localhost"))
-                return false;
-            return true;
-        }
+				string[] raw = Download (source);
 
-        /// <summary>
-        /// Cleans the entry
-        /// </summary>
-        /// <param name="line">The string to clean</param>
-        /// <returns>The cleaned string</returns>
-        string Clean(string line)
-        {
-            return line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)[1].Replace("\r", "");
-        }
+				partial.Add (new List<string[]> ());
 
-        /// <summary>
-        /// Creates the header for the hosts file
-        /// </summary>
-        /// <returns>The header</returns>
-        string CreateHeader()
-        {
-            string header = "# Host file courtesy of SuperBonny.\n# This list was updated on " + DateTime.Now.ToShortDateString() + " at " + DateTime.Now.ToShortTimeString() + "\n# There are " + entries.Count + " hosts present.\n# These are the sources:\n#\n# ";
-            foreach (string s in Sources)
-            {
-                header += s + "\n# ";
-            }
-            header += "\n127.0.0.1 localhost\n::1 localhost\n\n# [START OF GENERATED ENTRIES]\n#\n";
-            return header;
-        }
+				partial [partial.Count - 1] = Parse (raw);
 
-        /// <summary>
-        /// Writes the file to disk
-        /// </summary>
-        /// <param name="filename">The path of the file</param>
-        /// <returns>If the operation succedded</returns>
-        public bool Write(string filename)
-        {
-            try
-            {
-                if (File.Exists(filename))
-                    File.Delete(filename);
-                StreamWriter sw = new StreamWriter(filename);
-                sw.Write(CreateHeader());
-                foreach (string[] host in entries)
-                    sw.Write(host[0] + "\t" + host[1] + "\n");
-                sw.Close();
-                Console.WriteLine(entries.Count + " entries written successfully.");
-                return true;
-            }
-            catch (UnauthorizedAccessException)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("You have no permission to write in " + filename + ", moron.");
-                Console.ResetColor();
-                return false;
-            }
-        }
-    }
+				Console.Write (Sources.IndexOf (source) + 1 + "/" + Sources.Count + " " + source + "...");
+
+			
+				//}));
+				//worker [worker.Count - 1].Start ();
+			}
+
+			/*foreach (Task t in worker) {
+				t.Wait ();
+			}*/
+
+			Console.Write ("\nMerging...");
+
+			foreach (List<string[]> list in partial) {
+				for (int i = 0; i < list.Count; i++) {
+					if (!Exists (entries, list [i])) {
+						entries.Add (list [i]);
+					}
+				}
+			}
+			Console.WriteLine ("Done.");
+
+			if (errors >= Sources.Count)
+				return false;
+			entries = entries.OrderBy (o => o [1]).ToList ();
+			return true;
+		}
+
+		string[] Download (string source)
+		{
+			WebClient cl = new WebClient ();
+			cl.Proxy.Credentials = CredentialCache.DefaultCredentials;
+			try {
+				return cl.DownloadString (source).Split ('\n');
+			} catch (WebException e) {
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.WriteLine (e.Message);
+				Console.ResetColor ();
+			}
+			return default(string[]);
+		}
+
+		List<string[]> Parse (string[] source)
+		{
+			List<string[]> outp = new List<string[]> ();
+			string[] entry;
+
+			for (int i = 0; i < source.Length; i++) {
+				if (isRelevant (source [i])) {
+					entry = new string[] { addr, Clean (source [i]) };
+					outp.Add (entry);
+				}
+			}
+			return outp;
+		}
+
+		/// <summary>
+		/// Check if the given host already exists in the given list
+		/// </summary>
+		/// <param name="list">The list to check</param>
+		/// <param name="arr">The element to verify</param>
+		/// <returns></returns>
+		bool Exists (List<string[]> list, string[] arr)
+		{
+			for (int i = 0; i < list.Count; i++) {
+				if (list [i] [1] == arr [1])
+					return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Checks wether the string given is a valid host entry
+		/// </summary>
+		/// <param name="line">The line to check</param>
+		/// <returns>If the line is valid</returns>
+		bool isRelevant (string line)
+		{
+			line = line.Replace (" ", ",");
+			if (line.StartsWith ("#") || line.Length == 0 || line.StartsWith ("\r") || line.Contains ("localhost"))
+				return false;
+			return true;
+		}
+
+		/// <summary>
+		/// Cleans the entry
+		/// </summary>
+		/// <param name="line">The string to clean</param>
+		/// <returns>The cleaned string</returns>
+		string Clean (string line)
+		{
+			return line.Split (new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries) [1].Replace ("\r", "");
+		}
+
+		/// <summary>
+		/// Creates the header for the hosts file
+		/// </summary>
+		/// <returns>The header</returns>
+		string CreateHeader ()
+		{
+			string header = "# Host file courtesy of SuperBonny.\n# This list was updated on " + DateTime.Now.ToShortDateString () + " at " + DateTime.Now.ToShortTimeString () + "\n# There are " + entries.Count + " hosts present.\n# These are the sources:\n#\n# ";
+			foreach (string s in Sources) {
+				header += s + "\n# ";
+			}
+			header += "\n127.0.0.1 localhost\n::1 localhost\n\n# [START OF GENERATED ENTRIES]\n#\n";
+			return header;
+		}
+
+		/// <summary>
+		/// Writes the file to disk
+		/// </summary>
+		/// <param name="filename">The path of the file</param>
+		/// <returns>If the operation succedded</returns>
+		public bool Write (string filename)
+		{
+			try {
+				if (File.Exists (filename))
+					File.Delete (filename);
+				StreamWriter sw = new StreamWriter (filename);
+				sw.Write (CreateHeader ());
+				foreach (string[] host in entries)
+					sw.Write (host [0] + "\t" + host [1] + "\n");
+				sw.Close ();
+				Console.WriteLine (entries.Count + " entries written successfully.");
+				return true;
+			} catch (UnauthorizedAccessException) {
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.WriteLine ("You have no permission to write in " + filename + ", moron.");
+				Console.ResetColor ();
+				return false;
+			}
+		}
+	}
 }
